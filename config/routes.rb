b6bb986e27617463722392df3f61c61927cd3aeb
prius2055 <prius2055@gmail.com>
2023-08-15 13:32:52 +0100

    setup devise for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  # Defines the root path route ("/")
+  root "expenses#index"
+
   resources :expenses
   resources :expenditures
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
