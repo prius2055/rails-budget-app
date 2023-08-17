a767661a8d79eac6c510d6ae0a5c22036684f04b
prius2055 <prius2055@gmail.com>
2023-08-17 19:50:44 +0100

    Add test

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
 
   # Defines the root path route ("/")
-  root "expenses#index"
+
+  authenticated :user do
+    root 'expenses#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root "homepage#index", as: :unauthenticated_root
+  end
 
   resources :expenses
   resources :expenditures
   resources :users
-
-  get '/homepage', to: 'homepage#index'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 end
---
+++ spec/models/expenditure_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe Expenditure, type: :model do
+   describe ' #index' do
+    let(:first_user) { User.create(name: 'Tom') }
+    let(:first_expense) do
+      Expense.create(author: first_user, name: 'Apple', amount: 2000)
+    end
+
+
+    it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'amount should be less than 251 ch' do
+    subject.amount = nil
+    expect(subject).to_not be_valid
+  end
+  end
+end
