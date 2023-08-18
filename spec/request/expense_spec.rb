a19d104729a739142af020352124ed3867cf8b01
prius2055 <prius2055@gmail.com>
2023-08-18 17:31:41 +0100

    Fix linters error

--- spec/request/expense_spec.rb
+++ spec/request/expense_spec.rb
@@ -6,25 +6,23 @@ describe Expense, type: :request do
       @user = User.create(name: 'Leon', email: 'leon@gmail.com', password: '123456', password_confirmation: '123456')
       @expense = Expense.create(name: 'KFC', icon: 'https://unsplash.com/photos/F_-0BxGuVvo', author_id: @user.id)
     end
-    
+
     it 'should return a successful response' do
-    get expenses_path(@user)
-    expect(response).to be_successful
+      get expenses_path(@user)
+      expect(response).to be_successful
     end
 
     it 'should render user name' do
-    expect(@user.name).to eq('Leon')
+      expect(@user.name).to eq('Leon')
     end
 
     it 'should render category name' do
-    expect(@expense.name).to eq('KFC')
+      expect(@expense.name).to eq('KFC')
     end
 
     it 'renders the index template' do
       get expenses_path(@user)
       expect(response).to render_template(:index)
     end
-
   end
-
 end
